3de1a4ac0126a74ef90a7946be0b674e68e84c1d
Raghvendra <raghvendrapratap678@gmail.com>
2025-01-12 18:54:39 +0530

    commit this project

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const routes = require("./routes");
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://mongo:27017/todos", {
+  await mongoose.connect("mongodb://mongoUser:mongoPassword@mongo:27017/my-database?authSource=admin", {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });
